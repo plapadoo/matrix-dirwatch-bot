d465cf63a0b69f2037d0f55fc4e2e870682b95f9
Philipp Middendorf <middendorf@plapadoo.de>
2018-03-12 11:25:01 +0100

    default.nix: pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
 let
+  bootstrap = import <nixpkgs> { };
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs-channels";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src { };
+
   drv = pkgs.haskellPackages.callCabal2nix "matrix-dirwatch" ./. {};
 in
-  if pkgs.lib.inNixShell then drv.env else drv
+  if pkgs.lib.inNixShell then drv.env else pkgs.haskell.lib.justStaticExecutables drv
